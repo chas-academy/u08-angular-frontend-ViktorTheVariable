e72309ccfe08b8794d462b4899c46b9fb447b7a4
Viktor Ekstrom <viktor.ekstrom@chasacademy.se>
2025-05-07 22:33:33 +0200

    add number instances of input-field component. Modified logic to handle numbers.

--- top-war-movies/src/app/components/create-movie/create-movie.component.ts
+++ top-war-movies/src/app/components/create-movie/create-movie.component.ts
@@ -12,46 +12,48 @@ import { TextareaFieldComponent } from '../textarea-field/textarea-field.compone
   styleUrl: './create-movie.component.scss'
 })
 export class CreateMovieComponent {
-  // Formfields
   title: string = '';
   plot: string = '';
-  releaseYear?: number;
+  releaseYear: string = '';
   director: string = '';
   writers: string = '';
   actors: string = '';
   length: string = '';
   warType: string = '';
-  userRating?: number;
-  expertRating?: number;
+  userRating: string = '';
+  expertRating: string = '';
   language: string = '';
   country: string = '';
   imageUrl: string = '';
   trailerUrl: string = '';
 
-  // Feedback for user.
   successMessage: string = '';
   errorMessage: string = '';
 
   constructor(private movieService: MovieService) {}
 
   onSubmit() {
-    // Create new movie-object. If fields are empty, they will not be sent.
     const newMovie: any = {
       title: this.title
     };
-
+  
     if (this.plot) newMovie.plot = this.plot;
-    if (this.releaseYear) newMovie.releaseYear = this.releaseYear;
+    if (this.releaseYear && !isNaN(Number(this.releaseYear))) newMovie.releaseYear = Number(this.releaseYear);
     if (this.director) newMovie.director = this.director;
     if (this.writers) newMovie.writers = this.writers.split(',').map(s => s.trim());
     if (this.actors) newMovie.actors = this.actors.split(',').map(s => s.trim());
     if (this.length) newMovie.length = this.length;
     if (this.warType) newMovie.warType = this.warType;
-    if (this.userRating || this.expertRating) {
+  
+    const userRatingNum = this.userRating && !isNaN(Number(this.userRating)) ? Number(this.userRating) : undefined;
+    const expertRatingNum = this.expertRating && !isNaN(Number(this.expertRating)) ? Number(this.expertRating) : undefined;
+  
+    if (userRatingNum !== undefined || expertRatingNum !== undefined) {
       newMovie.imdbRating = {};
-      if (this.userRating) newMovie.imdbRating.userRating = this.userRating;
-      if (this.expertRating) newMovie.imdbRating.expertRating = this.expertRating;
+      if (userRatingNum !== undefined) newMovie.imdbRating.userRating = userRatingNum;
+      if (expertRatingNum !== undefined) newMovie.imdbRating.expertRating = expertRatingNum;
     }
+  
     if (this.language) newMovie.language = this.language.split(',').map(s => s.trim());
     if (this.country) newMovie.country = this.country.split(',').map(s => s.trim());
     if (this.imageUrl || this.trailerUrl) {
@@ -59,29 +61,28 @@ export class CreateMovieComponent {
       if (this.imageUrl) newMovie.media.imageUrl = this.imageUrl;
       if (this.trailerUrl) newMovie.media.trailerUrl = this.trailerUrl;
     }
-
+  
     this.movieService.createMovie(newMovie).subscribe({
       next: (result) => {
         this.successMessage = 'Movie created!';
         this.errorMessage = '';
-        // Restore form if you want.
         this.title = '';
         this.plot = '';
-        this.releaseYear = undefined;
+        this.releaseYear = '';
         this.director = '';
         this.writers = '';
         this.actors = '';
         this.length = '';
         this.warType = '';
-        this.userRating = undefined;
-        this.expertRating = undefined;
+        this.userRating = '';
+        this.expertRating = '';
         this.language = '';
         this.country = '';
         this.imageUrl = '';
         this.trailerUrl = '';
       },
       error: (err) => {
-        this.successMessage = 'Movie created successfully.';
+        this.successMessage = '';
         this.errorMessage = 'Something went wrong: ' + (err?.error?.message || err.message || err);
       }
     });
